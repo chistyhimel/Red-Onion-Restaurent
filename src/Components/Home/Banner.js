099ec1c5e1667f4ba977c517782767c55d26e838
chistyhimel <fc_himel@yahoo.com>
2020-11-21 14:49:44 +0600

    bg fixed

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 const Banner = () => {
-    return (
-        <div>
-        <div className="banner mb-md-5">
-          <div className="banner-box">
-            <h1 className="font-weight-bold pb-4 banner-title">Best Food Waiting for Your Belly</h1>
-            <div className="row">
-                <div className="col-md-9">
-                      <input type="text" className="form-control form-control-lg mb-3"/>
+  return (
+    <div>
+      <div className="banner">
+        <div className="banner-box">
+          <h1 className="banner-title text-center">
+            Best food waiting for your belly
+          </h1>
+              <div class="input-group search-box input-group-lg text-center">
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Search food items..."
+                />
+                <div class="input-group-append">
+                  <span class="btn btn-lg btn-danger " id="basic-addon2">
+                    Search
+                  </span>
                 </div>
-                <div className="col-md-3">
-                      <button className="btn btn-danger form-control btn-lg">Search</button>
-                </div>
-            </div>
-          </div>
+              </div>
+            
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Banner;
